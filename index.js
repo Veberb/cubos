c2a236ff817ec7a9bf0e2cfbff5471513087d8b1
Lucas <lucasveberdebrida@gmail.com>
2019-04-28 17:04:50 -0300

    Adicionando novas dependencias

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const express = require("express");
-const Boom = require("boom");
-const glob = require("glob");
-const cors = require("cors");
-const config = require("./config");
+const express = require('express')
+const Boom = require('boom')
+const glob = require('glob')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const config = require('./config')
 
-const app = express();
+const app = express()
+app.use(cors('*'))
+app.use(bodyParser.json({ limit: '50mb', type: 'application/json' }))
 
-app.use(cors("*"));
-
-const apis = glob.sync(`${config.path}/**/*Api.js`);
+const apis = glob.sync(`${config.path}/**/*Api.js`)
 
 apis.forEach(apiPath => {
-  require(`${apiPath}`)(app);
-});
+	require(`${apiPath}`)(app)
+})
 
-app.use((err, req, res, next) => {
-  const error = Boom.isBoom(err) ? err : Boom.boomify(err);
-  res
-    .status(error.output.statusCode)
-    .json({ message: error.message, error: error.output.payload.error });
-});
+app.use((err, req, res) => {
+	const error = Boom.isBoom(err) ? err : Boom.boomify(err)
+	res
+		.status(error.output.statusCode)
+		.json({ message: error.message, error: error.output.payload.error })
+})
 
-app.listen(3000);
+app.listen(3000)
